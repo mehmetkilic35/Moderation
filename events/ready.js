28e5c2e07e2ca705daeba951ba7620dd55425809
Glitch (cloudy-bolder-snow) <none>
2022-08-10 20:47:50 +0000

    💪👲 Checkpoint ./events/ready.js:24450525/205 ./bot.js:24450525/3352

--- events/ready.js
+++ events/ready.js
@@ -9,13 +9,6 @@ module.exports = client => {
 var oyun = [
         "♾️ MUSTAFA KEMAL ATATÜRK"
     ];
-
-    setInterval(function() {
-
-        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
-
-        client.user.setActivity(oyun[random], "https://bionluk.com/pealdigital" );
-        }, 2 * 2500);
     
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
   console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
