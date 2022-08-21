cf9c2af6b514edb64fb54f414b2872fdf47a0d6a
Glitch (cloudy-bolder-snow) <none>
2022-08-21 16:34:28 +0000

    🌍🚶 Checkpoint ./komutlar/iletişim.js:24450525/1181

--- komutlar/iletişim.js
+++ komutlar/iletişim.js
@@ -5,9 +5,9 @@ exports.run = async(client, message) => {
 	let prefix = '*'
  message.delete();
 	const peladigital = new Discord.RichEmbed()
-       .setAuthor(`🔱 KılıçBot | İletişim 🔱`)
+       .setAuthor(`🔱 Mehmet Kılıç İletişim 🔱`)
        .setColor(`PURPLE`)
-       .setDescription("\n 📌 **İletişim Bilgileri** \n \n <:gta:1004442247930052658> Gta5 ➜ `k!gta` \n <:VALORANT:1004442272282194010> Valorant ➜ `k!valorant` \n <:AMOUGUS:1004442179210596362> Amogus ➜ `k!amongus`\n <:MINECRAFT:1004442383812919306> Minecraft ➜ `k!mc`\n <:LOL:1004442413802193008> Lol ➜ `k!lol`\n <:FIVEM:1004442326707482815> FiveM ➜ `k!fivem`\n <:R6:1004442116623179826> R6 ➜ `k!r6`\n <:APEX:1004442046452482098> Apex ➜ `k!apex`\n <:CSGO:1004442009701994607> CsGo ➜ `k!cs`\n <:ARK:1004442301579403274> Ark ➜ `k!ark`\n <:FORTNTE:1004442092740804749> Fortnite ➜ `k!fortnite`\n <:ROBLOX:1004442353370661004> Roblox ➜ `k!roblox`\n <:DOTA2:1004442202346377226> Dota2 ➜ `k!dota`\n <:WOT:1004442139247259681> World of Tanks ➜ `k!wot`\n <:WTCHER3:1004442070452277450> Witcher3 ➜ `k!witcher`")
+       .setDescription("\n <a:tickgreen:1004423035891232849> **İletişim Bilgileri** \n \n <a:welcome:1010947107693264987> **WebSite** ➜ https://mehmetkilicoffical.com/ \n <a:Discord:995830729344811128> **Discord** ➜ https://discord.gg/mehmetkilic \n <a:Instagram:995830741176942682> **Instagram** ➜ https://instagram.com/_mehmetkilic \n ")
        .addField("KılıçModeration", `  [PealDigital](https://bionluk.com/pealdigital)`, false)
   return message.channel.send(peladigital)
 }
